1cee4008edbc68002c7ad098bd9bad87b21c42ee
Vikas Vishnoi <vikas.vshn@gmail.com>
2019-09-22 13:11:57 +0800

    initial commit Signed-off-by: Vikas Vishnoi <vikas.vshn@gmail.com>

---
+++ src/main/java/com/crudlearn/mysqlcrud/SpringBootRestJpaMySqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.crudlearn.mysqlcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestJpaMySqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestJpaMySqlCrudApplication.class, args);
+	}  
+
+}
